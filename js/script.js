4904a40a73c9c05d61454ecaceb7c9a1137e2830
DanielPetelin <daniilpetelin77@gmail.com>
2020-04-13 14:46:32 +0300

    add map

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
-function initMap {
-    
-}
+function initMap() {
+    let opt = {
+        center: {lat:47.210663, lng: 38.922561},
+        zoom: 4
+    };
+    let map = new google.maps.Map(document.querySelector('#map'), opt);
+};
