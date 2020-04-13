36e3e57d4e0cd158d72d362060e52e7edfc6f9f9
DanielPetelin <daniilpetelin77@gmail.com>
2020-04-13 15:35:07 +0300

    add final

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 function initMap() {
+    let pos = {lat:47.210663, lng: 38.922561};
     let opt = {
-        center: {lat:47.210663, lng: 38.922561},
-        zoom: 4
+        center: pos,
+        zoom: 17,
     };
-    let map = new google.maps.Map(document.querySelector('#map'), opt);
+    let myMap = new google.maps.Map(document.querySelector('#map'), opt);
+
+    let marker = new google.maps.Marker ({
+        position: pos,
+        map: myMap,
+
+    });
 };
